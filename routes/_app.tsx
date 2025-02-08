3517bedf9681bd8e94802e73484bb47772210a98
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2025-02-08 20:07:40 -0300

    chore: importa fontes

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,7 +5,16 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>anapaulacavalcanti</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin={"true"}
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Commissioner:wght@100..900&family=Tangerine:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,12 +6,11 @@ export default {
   ],
   theme: {
     colors: {
-      "primary": "#F5A7B8",
-      "dusty-rose": "#D6A5A1",
-      "blush-pink": "#F8D1D1",
-      "soft-mauve": "#D2A5D8",
-      "peachy-pink": "#FAD0C9",
-      "warm-ivory": "#F4E1D2",
+    extend: {
+      fontFamily: {
+        "bonita": ["Tangerine", "serif"],
+        "formal": ["Commissioner", "serif"],
+      },
     },
   },
 } satisfies Config;
