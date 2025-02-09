5330062e8b8b7b25d9d03b1d1dbb3929ab3756d2
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2025-02-09 08:57:35 -0300

    chore: define cores

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -17,7 +17,7 @@ export default function App({ Component }: PageProps) {
         />
         <link rel="stylesheet" href="/styles.css" />
       </head>
-      <body>
+      <body class="font-formal bg-corDeFundo">
         <Component />
       </body>
     </html>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,15 @@ export default {
   ],
   theme: {
     colors: {
+      "corPrimaria": "#F5A7B8",
+      "corSecundaria": "#D2A5D8",
+      "corContrastante": "#FAD0C9",
+      "corDeFundo": "#F4E1D2",
+      "corParaBotoes": "#D6A5A1",
+      "corDeDestaque": "#F8D1D1",
+      "corClaraParaTexto": "#F7F7F7",
+      "corEscuraParaTexto": "#333333",
+    },
     extend: {
       fontFamily: {
         "bonita": ["Tangerine", "serif"],
